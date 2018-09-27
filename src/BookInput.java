3d359b0f5ea6dd186b1c9a5602d509d124865159
Nigelitt <43179051+Nigelitt@users.noreply.github.com>
2018-09-27 15:50:16 +0100

    Lab3

--- src/BookInput.java
+++ src/BookInput.java
@@ -3,7 +3,11 @@ import java.awt.*;
 
 public class BookInput {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        StringBuilder allValidCombos= new StringBuilder();
+        boolean valid;
+        int digitCount=0,j;
 
         Book likeBook = new Book();
         Book dislikeBook = new Book();
@@ -26,6 +30,13 @@ public class BookInput {
 
         int bookInput = JOptionPane.showConfirmDialog(null, bookPanel,
                 "Favourite book", JOptionPane.OK_CANCEL_OPTION);
+        if (bookInput == JOptionPane.CANCEL_OPTION)
+        {
+            JOptionPane.showMessageDialog(null, "Error");
+            System.exit(0);
+        }
+
+
 
         String title = titleField.getText();
         likeBook.setTitle(title);
@@ -35,6 +46,9 @@ public class BookInput {
         likeBook.setNumPages(numpages);
 
         String isbn = isbnField.getText();
+        valid=false;
+
+        isbn = getString(allValidCombos, valid, isbn);
         likeBook.setIsbn(isbn);
 
         String price1 = priceField.getText();
@@ -59,6 +73,11 @@ public class BookInput {
 
         int bookInput2 = JOptionPane.showConfirmDialog(null, bookPanel2,
                 "Least favourite", JOptionPane.OK_CANCEL_OPTION);
+        if (bookInput2 == JOptionPane.CANCEL_OPTION)
+        {
+            JOptionPane.showMessageDialog(null, "Error");
+            System.exit(0);
+        }
 
         String title1 = titleField2.getText();
         dislikeBook.setTitle(title1);
@@ -68,23 +87,57 @@ public class BookInput {
         dislikeBook.setNumPages(numpages3);
 
         String isbn1 = isbnField2.getText();
+        valid=false;
+
+        isbn1 = getString(allValidCombos, valid, isbn1);
         dislikeBook.setIsbn(isbn1);
 
         String price2 = priceField2.getText();
         double price3 = Double.parseDouble(price2);
         dislikeBook.setPrice(price3);
 
-        if (bookInput == JOptionPane.OK_OPTION && bookInput2 == JOptionPane.OK_OPTION) {
+        if (bookInput == JOptionPane.OK_OPTION && bookInput2 == JOptionPane.OK_OPTION)
+        {
             JTextArea textArea = new JTextArea();
 
             textArea.setText(String.format( "%-20s %10s  \n\n %-20s %10s ","Favourite book",
-                     dislikeBook.toString(), "Least Favourite",likeBook.toString()));
+                    likeBook.toString() , "Least Favourite", dislikeBook.toString()));
 
             JOptionPane.showMessageDialog(null, textArea);
         }
-        else if (bookInput == JOptionPane.CANCEL_OPTION) {
-            JOptionPane.showMessageDialog(null, "Error");
-        }
 
+
+    }
+
+    public static String getString(StringBuilder allValidCombos, boolean valid, String isbn1) {
+        int digitCount;
+        int j;
+        while(!valid)
+        {
+            digitCount=0;
+            if(isbn1.length()>=10 && isbn1.length()<=13)
+            {
+                for(j=0;j<isbn1.length();j++)
+                    if(!Character.isDigit(isbn1.charAt(j)) && !Character.isLetter(isbn1.charAt(j)))
+                        break;
+                    else if(Character.isDigit(isbn1.charAt(j)))
+                        digitCount++;
+
+                if(j==isbn1.length())
+                    if(digitCount>=3)
+                    {
+                        valid = true;
+                        allValidCombos.append(isbn1);
+                    }
+                    else
+                        isbn1 = JOptionPane.showInputDialog("Must contain at least 3 digits! Please re-enter " );
+                else
+                    isbn1 = JOptionPane.showInputDialog("Letters and digits only! Please re-enter " );
+
+            }
+            else
+                isbn1 = JOptionPane.showInputDialog("Invalid length! Please re-enter ");
+        }
+        return isbn1;
     }
 }
